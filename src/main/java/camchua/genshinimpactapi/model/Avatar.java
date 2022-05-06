bdc8d1e8fc078e6da144f64fc317c696f4d2ada8
CamChua_VN <32415041+CamChuaVN@users.noreply.github.com>
2022-05-06 12:41:06 +0000

    Genshin Impact API

--- src/main/java/camchua/genshinimpactapi/model/Avatar.java
+++ src/main/java/camchua/genshinimpactapi/model/Avatar.java
@@ -3,51 +3,52 @@ package camchua.genshinimpactapi.model;
 import camchua.genshinimpactapi.enums.ElementEnum;
 
 public class Avatar {
-	
+
 	private String ownedId;
 	private String avatarId;
 	private String name;
-    private ElementEnum element;
-    private int rarity;
-    private int fetter;
-    private int level;
-    
-    public Avatar(String ownedId, String avatarId, String name, ElementEnum element, int rarity, int fetter, int level) {
-    	this.ownedId = ownedId;
-    	this.avatarId = avatarId;
-    	this.name = name;
-    	this.element = element;
-    	this.rarity = rarity;
-    	this.fetter = fetter;
-    	this.level = level;
-    }
-    
-    public String getOwnedId() {
-    	return this.ownedId;
-    }
-    
-    public String getAvatarId() {
-    	return this.avatarId;
-    }
-    
-    public String getName() {
-    	return this.name;
-    }
-    
-    public ElementEnum getElement() {
-    	return this.element;
-    }
-    
-    public int getRarity() {
-    	return this.rarity;
-    }
-    
-    public int getFetter() {
-    	return this.fetter;
-    }
-    
-    public int getLevel() {
-    	return this.level;
-    }
+	private ElementEnum element;
+	private int rarity;
+	private int fetter;
+	private int level;
+
+	public Avatar(String ownedId, String avatarId, String name, ElementEnum element, int rarity, int fetter,
+			int level) {
+		this.ownedId = ownedId;
+		this.avatarId = avatarId;
+		this.name = name;
+		this.element = element;
+		this.rarity = rarity;
+		this.fetter = fetter;
+		this.level = level;
+	}
+
+	public String getOwnedId() {
+		return this.ownedId;
+	}
+
+	public String getAvatarId() {
+		return this.avatarId;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public ElementEnum getElement() {
+		return this.element;
+	}
+
+	public int getRarity() {
+		return this.rarity;
+	}
+
+	public int getFetter() {
+		return this.fetter;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
 
 }
--- src/main/java/camchua/genshinimpactapi/model/Player.java
+++ src/main/java/camchua/genshinimpactapi/model/Player.java
@@ -3,25 +3,25 @@ package camchua.genshinimpactapi.model;
 import java.util.List;
 
 public class Player {
-	
+
 	private String uid;
 	private List<Avatar> avatars;
 	private Stat stat;
-	
+
 	public Player(String uid, List<Avatar> avatars, Stat stat) {
 		this.uid = uid;
 		this.avatars = avatars;
 		this.stat = stat;
 	}
-	
+
 	public String getUid() {
 		return this.uid;
 	}
-	
+
 	public List<Avatar> getAvatars() {
 		return this.avatars;
 	}
-	
+
 	public Stat getStat() {
 		return this.stat;
 	}
